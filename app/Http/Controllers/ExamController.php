7ee9e02fcdd071e61921f794b61a983eaa7115ac
3cfish <40749453+3cfish@users.noreply.github.com>
2018-07-04 15:57:00 +0800

    路由模型綁定

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -47,9 +47,9 @@ class ExamController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Exam $exam)
     {
-        $exam = Exam::find($id);
+
         return view('exam.show', compact('exam'));
     }
 
